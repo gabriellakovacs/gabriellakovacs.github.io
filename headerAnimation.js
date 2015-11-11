d013488076011c5e946ef13797670e13dbc9caa6
gabriella kovacs <kovacsxgabriella@gmail.com>
2015-11-11 19:15:49 +0000

    modify js  so that functions only fire once the document has been parsed

--- headerAnimation.js
+++ headerAnimation.js
@@ -1,38 +1,41 @@
-// move navigational circles to left side by adding sticky class
-var stickyHeader = $('.nav').offset().top;
+$(function(){
+    // move navigational circles to left side by adding sticky class
+    var stickyHeader = $('.nav').offset().top;
 
-$(window).scroll(function() {  
-    if ($(window).scrollTop() > stickyHeader) {
-        $('.nav').addClass('sticky');
-    }
-    else {
-        $('.nav').removeClass('sticky');
-    }  
-});
+    $(window).scroll(function() {  
+        if ($(window).scrollTop() > stickyHeader) {
+            $('.nav').addClass('sticky');
+        }
+        else {
+            $('.nav').removeClass('sticky');
+        }  
+    });
 
-//color active navigational circle by adding active class
+    //color active navigational circle by adding active class
 
-var porfolio = $('#portfolio').offset().top;
-var about = $('#about').offset().top;
-var contact = $('#contact').offset().top;
+    var porfolio = $('#portfolio').offset().top;
+    var about = $('#about').offset().top;
+    var contact = $('#contact').offset().top;
 
-$(window).scroll(function() {  
-    if ($(window).scrollTop() > porfolio && $(window).scrollTop() < about) {
-        $('#navPortfolio').addClass('active');
-    }
-    else {
-        $('#navPortfolio').removeClass('active');
-    }
-    if ($(window).scrollTop() > about && $(window).scrollTop() < contact) {
-        $('#navAbout').addClass('active');
-    }
-    else {
-        $('#navAbout').removeClass('active');
-    }
-    if ($(window).scrollTop() > contact) {
-        $('#navContact').addClass('active');
-    }
-    else {
-        $('#navContact').removeClass('active');
-    } 
+    $(window).scroll(function() { 
+
+        if ($(window).scrollTop() > porfolio && $(window).scrollTop() < about) {
+            $('#navPortfolio').addClass('active');
+        }
+        else {
+            $('#navPortfolio').removeClass('active');
+        }
+        if ($(window).scrollTop() > about && $(window).scrollTop() < contact) {
+            $('#navAbout').addClass('active');
+        }
+        else {
+            $('#navAbout').removeClass('active');
+        }
+        if ($(window).scrollTop() > contact) {
+            $('#navContact').addClass('active');
+        }
+        else {
+            $('#navContact').removeClass('active');
+        } 
+    });
 });
